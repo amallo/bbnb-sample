021d7c271d6ac8351bd44ea565aa9a91d3a84b81
Audie MALLOGGIA [APP2B] <audie@app2b.io>
2020-04-22 14:45:14 +0200

    feature(exo): use one loading spinner for the app

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import React, {Component} from 'react';
-import Navigation from './src/navigation';
+import React, { Component } from 'react';
+import MainScreen from './src/screens/Main';
 import reducers from './src/reducers';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 const store = createStore(reducers, composeWithDevTools());
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Navigation />
+        <MainScreen />
       </Provider>
     );
   }
