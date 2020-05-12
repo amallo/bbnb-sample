dc22e1aed2cceb4c62e6b9224fda56133c651123
Audie MALLOGGIA [APP2B] <audie@app2b.io>
2020-05-13 00:27:13 +0200

    refactor: move to redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import { composeWithDevTools } from 'redux-devtools-extension';
-
 import reducers from '../reducers';
+import thunk from 'redux-thunk';
 
 // On déclare le moteur de stockage utilisé par redux-persist
 import AsyncStorage from '@react-native-community/async-storage';
@@ -11,9 +11,15 @@ const persistConfig = {
     key: 'root',
     storage: AsyncStorage,
 }
-const persistedReducer = persistReducer(persistConfig, reducers)
+
+// On liste ici les middleware dont on a besoin
+//
+const middlewares = [thunk]
 
 // composeWithDevTools() permet le debuggage dans React Native Debugger
 // 
-export const store = createStore(persistedReducer, __DEV__ ? composeWithDevTools() : undefined);
+const enhancers = [composeWithDevTools({})(applyMiddleware(...middlewares))]
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+export const store = createStore(persistedReducer, compose(...enhancers));
 export const persistor = persistStore(store)
